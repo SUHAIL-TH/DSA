ca3ac13f580c94f56af327e29e616f02ae4384cf
Suhail TH <suhailth17756@gmail.com>
2023-05-23 18:14:33 +0530

    trie dsa

---
+++ week 15/last.js
@@ -0,0 +1,46 @@
+class TrieNode{
+    constructor(){
+        this.map=new Map()
+    }
+}
+
+var endSymbol="*"
+var root=new TrieNode()
+
+class Trie{
+    psuffixtrie(str){
+        for(var i=0;i<str.length;i++){
+            this.insert(i,str)
+        }
+    }
+
+    insert(index,str){
+        var node=root
+        for(var j=index;j<str.length;j++){
+            let ltr=str.charAt(j)
+            var newNode=new TrieNode()
+            if(!node.map.has(ltr)){
+               
+                node.map.set(ltr,newNode)
+            }
+            node=node.map.get(ltr)
+        }
+        node.map.set(endSymbol,null)
+    }
+
+    contain(str){
+        var node=root
+        for(var i=0;i<str.length;i++){
+            var letter=str.charAt(i)
+            if(!node.map.has(letter)){
+                return false
+            }
+            node=node.map.get(letter)
+        }
+        return node.map.has(endSymbol)
+    }
+}
+
+const t=new Trie()
+t.psuffixtrie("mannan")
+console.log(t.contain("an"));
