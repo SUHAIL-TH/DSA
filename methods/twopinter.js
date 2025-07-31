f38279c61a7ea21bc4c86c2b19abdedbe0a14c01
suhail <suhailth17756@gmail.com>
2025-07-31 16:56:37 +0530

    two pointer

---
+++ methods/twopinter.js
@@ -0,0 +1,26 @@
+//this contain the code for  doing two pinter
+
+//Write the code for reverse an array usin gtow pointer?
+
+
+let array=[1,2,3,4,5,6]
+
+function reversearray(array){
+    let left=0
+    let right=array.length-1
+    console.log(right)
+    while(left<right){
+        let temp=array[left]
+        array[left]=array[right]
+        array[right]=temp
+
+        //increment the pointers
+        left++
+        right--
+    }
+    return array
+
+}
+
+let data=reversearray(array)
+console.log(data)//reveresed array data
